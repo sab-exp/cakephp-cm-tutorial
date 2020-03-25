adb6e923d4cfb077b81cec36a2d36f5c642aa67d
Sabbir Ahmed <sabbir_id@outlook.com>
2020-03-25 16:04:24 +0900

    Add Edit Action

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -18,8 +18,8 @@ class ArticlesController extends AppController
         $this->set(compact('article'));
     }
     
-    public function add()
-    {
+    public function add() {
+        
         $article = $this->Articles->newEmptyEntity();
         if ($this->request->is('post')) {
             $article = $this->Articles->patchEntity($article, $this->request->getData());
@@ -36,4 +36,23 @@ class ArticlesController extends AppController
         }
         $this->set('article', $article);
     }
+
+    public function edit($slug) {
+
+        $article = $this->Articles
+            ->findBySlug($slug)
+            ->firstOrFail();
+    
+        if ($this->request->is(['post', 'put'])) {
+            $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been updated.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to update your article.'));
+        }
+    
+        $this->set('article', $article);
+    }
+
 }
