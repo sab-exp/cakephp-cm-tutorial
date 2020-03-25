9e2f4413e1e42fd143631af29f71c60aa1bb8a69
Sabbir Ahmed <sabbir_id@outlook.com>
2020-03-25 12:56:51 +0900

    Articles Controller

---
+++ src/Controller/ArticlesController.php
@@ -0,0 +1,14 @@
+<?php
+// src/Controller/ArticlesController.php
+
+namespace App\Controller;
+
+class ArticlesController extends AppController
+{
+    public function index()
+    {
+        $this->loadComponent('Paginator');
+        $articles = $this->Paginator->paginate($this->Articles->find());
+        $this->set(compact('articles'));
+    }
+}
