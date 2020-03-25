8bb12b10586ff78f9cead8a193929a57493235fc
Sabbir Ahmed <sabbir_id@outlook.com>
2020-03-25 14:42:37 +0900

    Create the View Action

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -11,4 +11,10 @@ class ArticlesController extends AppController
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));
     }
+    
+    public function view($slug = null)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
+    }
 }
